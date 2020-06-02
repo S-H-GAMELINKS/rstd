c09964532b89f1bde2aa87758134213c48d095fe
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-03 01:25:19 +0900

    add String#first test #7

--- test/rstd/rstd_string_test.rb
+++ test/rstd/rstd_string_test.rb
@@ -7,4 +7,27 @@ class RstdStringTest < Minitest::Test
   def setup
     @string = "Hello World!"
   end
+
+  def test_rstd_string_first
+    # Get the string slice(begin to string head)
+    result = @string.first
+    assert_equal "H", result
+    assert_kind_of String, result
+    
+    result = @string.first(3)
+    assert_equal "Hel", result
+    assert_kind_of String, result
+
+    result = @string.first(100)
+    assert_equal "Hello World!", result
+    assert_kind_of String, result
+
+    result = @string.first(-1)
+    assert_equal "Hello World!", result
+    assert_kind_of String, result
+
+    result = @string.first(-100)
+    assert_equal "", result
+    assert_kind_of String, result
+  end
 end
