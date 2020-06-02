06e995837aab61da6e9e1e0a285b34241c74325e
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-02 23:13:31 +0900

    fix Array test #3

--- test/rstd/rstd_array_test.rb
+++ test/rstd/rstd_array_test.rb
@@ -1,30 +1,31 @@
 require "test_helper"
 require "rstd"
 
+using Rstd
+
 class RstdArrayTest < Minitest::Test
   def setup
-    using Rstd::Array
-    ary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    @ary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   end
 
   def test_rstd_array_first_last
     # Get ary first and last value
-    result = ary.first_last
-    assert_equal [1, 10]
+    result = @ary.first_last
+    assert_equal [[1], [10]], result
 
     # Get ary first and last value's for get value count
-    result = ary.first_last(3)
+    result = @ary.first_last(3)
     assert_equal [1, 2, 3], result.first
     assert_equal [8, 9, 10], result.last
     assert_kind_of Array, result
 
     # Get ary first and last value reversed
-    result = ary.first_last(reversed: true)
-    assert_equal [10, 1], result
+    result = @ary.first_last(reversed: true)
+    assert_equal [[1], [10]], result
     assert_kind_of Array, result
 
     # Get ary first and last value's for get value count & rversed
-    result = ary.first_last(3, reversed: true)
+    result = @ary.first_last(3, reversed: true)
     assert_equal [3, 2, 1], result.first
     assert_equal [10, 9, 8], result.last
     assert_kind_of Array, result
