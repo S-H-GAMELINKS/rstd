7a1cac4dd40515d7d038dbe4d5960546708c577e
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-04 06:11:55 +0900

    add Integer#dec test #9

--- test/rstd/rstd_integer_test.rb
+++ test/rstd/rstd_integer_test.rb
@@ -18,4 +18,15 @@ class RstdIntegerTest < Minitest::Test
     assert_equal 44, result
     assert_kind_of Integer, result
   end
+
+  def test_rstd_integer_dec
+    # Decrement num
+    result = @num.dec
+    assert_equal 41, result
+    assert_kind_of Integer, result
+
+    result = @num.dec.dec
+    assert_equal 40, result
+    assert_kind_of Integer, result
+  end
 end
