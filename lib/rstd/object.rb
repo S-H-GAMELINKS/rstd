7c026b85887116fa58052420df602140e0b437e6
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-09-22 22:44:27 +0900

    Add Object#refined_method? method

--- lib/rstd/object.rb
+++ lib/rstd/object.rb
@@ -1,5 +1,17 @@
 module Rstd::RefineObject
   refine Object do
+    def refine_method?(method)
+      if method.is_a?(Symbol)
+        if self.respond_to?(method)
+          method_obj = self.method(method)
+          return method_obj.to_s =~ /refinement/ ? true : false
+        else
+          raise NotImplementedError, "#{method} is not implemented"
+        end
+      else
+        raise ArgumentError, "you can given arguments only Symbol"
+      end
+    end
     def present?
       true
     end
