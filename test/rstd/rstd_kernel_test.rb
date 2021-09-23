a579f540f06b8df97eeb9177ae1e3a2b0f4bfea8
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-09-23 19:41:46 +0900

    Add test

--- test/rstd/rstd_kernel_test.rb
+++ test/rstd/rstd_kernel_test.rb
@@ -4,7 +4,9 @@ require "rstd"
 using Rstd::RefineKernel
 
 class RstdKernelTest < Minitest::Test
-  using refine_method(Object, :halo, ->(args) { return args.first})
+  using refine_method(Object, :halo, ->(args) { return args})
+  using refine_method(Integer, :inc, ->() { self + 1})
+  using refine_method(Array, :pow, ->(num = 2){ self.map{|v| v ** num }})
 
   def test_rstd_kernel_refine_method
     # Dynamic refinements
@@ -21,5 +23,23 @@ class RstdKernelTest < Minitest::Test
     assert !result
     assert_equal false, result
     assert_kind_of FalseClass, result
+
+    int = 42
+
+    result = int.inc
+    assert_equal 43, result
+    assert_kind_of Integer, result
+
+    ary = [1, 2, 3]
+
+    result = ary.pow
+    assert_equal [1, 4, 9], result
+    assert_kind_of Array, result
+    assert_equal 3, result.size
+
+    result = ary.pow(3)
+    assert_equal [1, 8, 27], result
+    assert_kind_of Array, result
+    assert_equal 3, result.size
   end
 end
