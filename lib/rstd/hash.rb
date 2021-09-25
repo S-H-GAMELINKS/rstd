acdc6fbc8178df93beb7c80c0e7200ba40349e78
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-09-25 15:25:25 +0900

    Add Hash.refine_method

--- lib/rstd/hash.rb
+++ lib/rstd/hash.rb
@@ -1,5 +1,16 @@
 module Rstd::RefineHash
   refine Hash do
+
+    class << Hash
+      def refine_method(method_id, &expr)
+        Module.new do
+          refine Hash do
+            define_method(method_id, &expr)
+          end
+        end
+      end
+    end
+
     def has_pair?(key, value)
       has_key?(key) && has_value?(value)
     end
