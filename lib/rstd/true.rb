1faebbb13e2340bbf01bc8f17bd8d70732cedf2a
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-10-02 22:26:40 +0900

    Add TrueClass#refine_method

--- lib/rstd/true.rb
+++ lib/rstd/true.rb
@@ -1,5 +1,16 @@
 module Rstd::RefineTrue
   refine TrueClass do
+
+    class << TrueClass
+      def refine_method(method_id, &expr)
+        Module.new do
+          refine TrueClass do
+            define_method(method_id, &expr)
+          end
+        end
+      end
+    end
+
     def present?
       true
     end
