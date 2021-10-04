2d8f3c7a8220cdf55f20ba482e6f1d3380d0fb72
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-10-04 22:08:32 +0900

    Add FalseClass document

--- lib/rstd/false.rb
+++ lib/rstd/false.rb
@@ -1,9 +1,24 @@
 module Rstd::RefineFalse
+
+  # @!method present?()
+  #   +present?+ always return false.
+  #
+  #       v = false
+  #       v.present?
+  #       # => false
   refine FalseClass do
     def present?
       false
     end
+  end
 
+  # @!method blank?()
+  #   +blank?+ always return true.
+  #
+  #       v = false
+  #       v.blank?
+  #       # => true
+  refine FalseClass do
     def blank?
       true
     end
