1a382b2195e25f3a458c06ff7b8471c506df72e8
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-11-10 21:53:16 +0900

    Add document for Refined Hash methods

--- lib/rstd/hash.rb
+++ lib/rstd/hash.rb
@@ -1,6 +1,16 @@
 module Rstd::RefineHash
-  refine Hash do
 
+  # @!method refine_method()
+  # @param [Symbol] method_id define method symbol.
+  # @yield method implements.
+  # @return [Module] return refined module.
+  #
+  # @example Dynamic refine method.
+  #       using Hash.refine_method(:halo){ puts "halo" }
+  #       {}.halo
+  #       # => "halo"
+  # @!scope module
+  refine Hash do
     class << Hash
       def refine_method(method_id, &expr)
         Module.new do
@@ -10,7 +20,14 @@ module Rstd::RefineHash
         end
       end
     end
+  end
 
+  # @!method has_pair?()
+  #   +has_pair?+ check to if hash instance has key and value pair.
+  #
+  #       {:key => 42}.has_pair?(:key, 42)
+  #       # => true
+  refine Hash do
     def has_pair?(key, value)
       has_key?(key) && has_value?(value)
     end
