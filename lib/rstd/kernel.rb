242d3fc18933ca4732568650c71ac94ac016bbf8
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-01-11 19:35:42 +0900

    Add Kernel#import method implement

--- lib/rstd/kernel.rb
+++ lib/rstd/kernel.rb
@@ -8,4 +8,30 @@ module Rstd::RefineKernel
       end
     end
   end
+
+  # @!method import()
+  # @param [Symbol|String] Class name(symbol or string).
+  # @return [Array] return import Class's.
+  #
+  # @example import some class's.
+  #       import('json', 'JSON::State', 'JSON::Parser') in [c1, c2]
+  #       p c1.name
+  #       # => "JSON::Ext::Generator::State"
+  #       p c2.name
+  #       # => "JSON::Ext::Parser"
+  # @!scope module
+  refine Kernel do
+    def import(path, *import_classes)
+      raise "Kernel#import is not supported" if RUBY_VERSION > "2.4" && "2.7" > RUBY_VERSION 
+      require path
+
+      klasses = []
+
+      import_classes.each do |klass|
+        klasses.unshift Object.const_get(klass)
+      end
+
+      return klasses.reverse
+    end
+  end
 end
