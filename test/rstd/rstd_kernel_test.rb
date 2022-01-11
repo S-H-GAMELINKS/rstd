62eadc9149b0b3371bdc361153f8240d27200ac6
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-01-11 19:35:19 +0900

    Add Kernel#import method test

--- test/rstd/rstd_kernel_test.rb
+++ test/rstd/rstd_kernel_test.rb
@@ -42,4 +42,30 @@ class RstdKernelTest < Minitest::Test
     assert_kind_of Array, result
     assert_equal 3, result.size
   end
+
+  def test_rstd_kernel_import
+    if RUBY_VERSION > "2.4" && "2.7" > RUBY_VERSION
+      # Unsupported import method in Ruby 2.4 to 2.6
+      assert_raises RuntimeError do
+        import('csv', :CSV)
+      end
+    else
+      # Given symbol
+      result = import('csv', :CSV)
+      assert_equal 'CSV', result.first.name
+
+      # Givn string
+      result = import('csv', 'CSV')
+      assert_equal 'CSV', result.first.name
+
+      # Given multi string
+      results = import('json', 'JSON::State', 'JSON::Parser')
+
+      result1 = results[0]
+      result2 = results[1]
+
+      assert_equal 'JSON::Ext::Generator::State', result1.name
+      assert_equal 'JSON::Ext::Parser', result2.name
+    end
+  end
 end
